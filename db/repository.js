12489d1cc706b058e62ba53d7be90a2f9072ef9b
Yury Kargin <biome@list.ru>
2024-09-17 19:43:13 +0700

    feat: добавлены методы для выполнения сырого insert

--- db/repository.js
+++ db/repository.js
@@ -6,8 +6,12 @@ class Repository {
   }
 
   async insert({ data, cs }) {
-    if (!data.length) throw new Error("Нет данных для вставки");
-    const insert = this.pgp.helpers.insert(data, cs.insert);
+    this.logger.debug(
+      `Выполнение insert запроса: ${JSON.stringify(data)}; таблица: ${
+        cs.insert.table
+      }`
+    );
+    const insert = this.createInsertQuery({ data, cs });
     try {
       await this.db.none(insert);
     } catch (err) {
@@ -16,7 +20,45 @@ class Repository {
     }
   }
 
-  async create() {}
+  async rawInsert({ sql }) {
+    this.logger.debug(`Выполнение сырого insert запроса: ${sql}`);
+    try {
+      await this.db.none(sql);
+    } catch (err) {
+      this.logger.error("Произошла ошибка в бд: " + err);
+      throw err;
+    }
+  }
+
+  createInsertQuery({ data, cs }) {
+    return this.pgp.helpers.insert(data, cs.insert);
+  }
+
+  async tx() {}
+
+  async update({ data, cs, cond }) {
+    this.logger.debug(
+      `Выполнение update запроса: ${JSON.stringify(data)}; таблица: ${
+        cs.update.table
+      }; условие: ${cond}`
+    );
+    const where = this.pgp.as.format(
+      " WHERE " + cond.map((item) => `${item} = $\{${item}\}`).join(" "),
+      data
+    );
+    const update = this.pgp.helpers.update(data, cs.update) + where;
+    try {
+      await this.db.none(update);
+    } catch (err) {
+      this.logger.error("Произошла ошибка в бд: " + err);
+      throw err;
+    }
+  }
+
+  async select({ sql }) {
+    this.logger.debug(`Выполнение ручного запроса: ${sql}`);
+    return await this.db.any(sql);
+  }
 }
 
 module.exports = Repository;
