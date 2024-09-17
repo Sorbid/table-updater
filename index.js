c18df68624088280e5afc064de4b4e3e5a6ef7a8
Yury Kargin <biome@list.ru>
2024-09-17 19:47:38 +0700

    feat: добавлена загрузка настроек приложения из бд

--- index.js
+++ index.js
@@ -2,25 +2,52 @@ require("dotenv").config();
 const apis = require("./api");
 const config = require("./config");
 const logger = require("./utils/logger");
-const db = require("./db");
+const { db } = require("./db");
 // const dates = require("./utils/dates");
 
 const start = async () => {
-  Object.keys(apis).map(async (api) => {
-    const instance = new apis[api]({ logger, config, db });
-    const dates = {
-      startDate: new Date(Date.UTC(2024, 8, 9)),
-      endDate: new Date(Date.UTC(2024, 8, 12)),
-    };
-    console.log(dates);
-    // for (const date of dates) {
-    const data = await instance.start({
-      startDate: dates.startDate,
-      endDate: dates.endDate,
+  logger.info("Начало выполнения обновления");
+  const setting = db.Settings;
+
+  logger.debug("Получение настроек");
+  const settingData = await setting.select();
+
+  await Promise.all(
+    settingData.map(async (elem) => await updateEntity({ elem, setting }))
+  );
+  logger.info("Выполнение закончено");
+};
+
+const updateEntity = async ({ elem, setting }) => {
+  const { repository, start_date, end_date, upd_table_id, url } = elem;
+  const instance = new apis[repository]({ logger, db, config, url });
+  try {
+    await instance.start({
+      startDate: start_date,
+      endDate: end_date,
     });
-    // await db[api].insert(data);
-    // }
-  });
+    await setting.updateDate({
+      data: { lastUpdDate: end_date, upd_table_id },
+      cond: ["upd_table_id"],
+    });
+    await setting.insertLog({
+      data: {
+        upd_table_id,
+        isError: false,
+        errorText: undefined,
+        errorDate: undefined,
+      },
+    });
+  } catch (err) {
+    await setting.insertLog({
+      data: {
+        upd_table_id,
+        isError: true,
+        errorText: err.message,
+        errorDate: start_date,
+      },
+    });
+  }
 };
 
 start();
