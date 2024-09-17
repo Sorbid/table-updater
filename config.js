dbd982a7673e045a31e5780b122cc9d80ca79761
Yury Kargin <biome@list.ru>
2024-09-17 19:48:34 +0700

    fix: удаление лишних переменных

--- config.js
+++ config.js
@@ -1,16 +1,5 @@
-const {
-  DB_HOST,
-  DB_PORT,
-  DB_DATABASE,
-  DB_USER,
-  API_KEY,
-  DB_PASS,
-  END_DATE,
-  START_DATE,
-} = process.env;
-
-const currentDate = new Date().setUTCHours(0, 0, 0, 0);
-const yesterday = new Date(currentDate - 24 * 60 * 60 * 1000);
+const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, API_KEY, DB_PASS, LOG_LEVEL } =
+  process.env;
 
 const config = {
   DB_HOST,
@@ -19,11 +8,7 @@ const config = {
   DB_USER,
   DB_PASS,
   API_KEY,
-  LOG_LEVEL: "debug",
-  SELLER_ANALYTICS_URL: "https://seller-analytics-api.wildberries.ru/api/",
-  AD_URL: "https://advert-api.wildberries.ru",
-  endDate: new Date(END_DATE) || currentDate,
-  startDate: new Date(START_DATE) || yesterday,
+  LOG_LEVEL: LOG_LEVEL || "debug",
 };
 
 module.exports = config;
